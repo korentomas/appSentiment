6844c41b6c06c1765abc7570b21bb021c4ab1fbb
Tomas Pablo Korenblit <tomaskorenblit@gmail.com>
2022-01-12 18:28:41 -0300

    🗺️ INITIAL COMMIT

---
+++ streamlit.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+st.title('Sentiment Analysis G8 DigitalHouse')
+st.markdown('This app analyses the sentiment behind a sentence.')
+
+classifier = joblib.load('wellness-detection-model.pkl')
+
+def run():
+    with st.form(key='Type sentence'):
+        sentence = st.text_input('Type a sentence for which you want to know the sentiment')
+        submit_button = st.form_submit_button(label='Submit')
+        
+        if submit_button:
+            result = classifier(sentence)
+            st.write(result)
+ 
+
+if __name__=='__main__':
+    run()
